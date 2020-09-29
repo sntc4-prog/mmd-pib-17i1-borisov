1d64cb53aeb868a936c67a9c843c38db674ecb06
Egor Borisov <egorborisovzypo@gmail.com>
2020-09-29 12:41:35 +0600

    Added variables, constructor, method toString

--- lab4/src/Passenger.java
+++ lab4/src/Passenger.java
@@ -1,2 +1,20 @@
 public class Passenger {
+    String name;
+    String surName;
+    String secondName;
+    String phoneNumber;
+    String documentNumber;
+    String citizenship;
+
+    public Passenger(String name, String surName, String secondName, String phoneNumber, String documentNumber, String citizenship) {
+        this.name = name;
+        this.surName = surName;
+        this.secondName = secondName;
+        this.phoneNumber = phoneNumber;
+        this.documentNumber = documentNumber;
+        this.citizenship = citizenship;
+    }
+    public String toString(){
+        return "";
+    }
 }
