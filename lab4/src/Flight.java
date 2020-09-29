f3f9e640755818ebe93880a6b1cf8cc625256a29
Egor Borisov <egorborisovzypo@gmail.com>
2020-09-29 13:17:27 +0600

    Added variables, enum and constructor Added method toString in class Flight

--- lab4/src/Flight.java
+++ lab4/src/Flight.java
@@ -1,2 +1,23 @@
+import java.time.LocalDateTime;
+
 public class Flight {
+    String flightNumber;
+    Airplane airplane;
+    String airportDeparture;
+    String airportDestination;
+    LocalDateTime dateTimeDeparture;
+    LocalDateTime dateTimeArrival;
+
+    public Flight(String flightNumber, Airplane airplane, String airportDeparture, String airportDestination, LocalDateTime dateTimeDeparture, LocalDateTime dateTimeArrival) {
+        this.flightNumber = flightNumber;
+        this.airplane = airplane;
+        this.airportDeparture = airportDeparture;
+        this.airportDestination = airportDestination;
+        this.dateTimeDeparture = dateTimeDeparture;
+        this.dateTimeArrival = dateTimeArrival;
+    }
+    @Override
+    public String toString(){
+        return "Номер полета:"+ this.flightNumber + " Самолет:" + this.airplane + " Аэропорт отправления:"+ this.airportDeparture + " Аэропорт назначения:" + this.airportDestination + " Время отправления:"+this.dateTimeDeparture + " Время прибытия:"+ this.dateTimeArrival;
+    }
 }
