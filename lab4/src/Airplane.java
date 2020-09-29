e8fabceff410ec99cfa2093d3074b18e59511763
Egor Borisov <egorborisovzypo@gmail.com>
2020-09-29 13:09:28 +0600

    Added variables, enum and constructor Added method toString in class Airplane

--- lab4/src/Airplane.java
+++ lab4/src/Airplane.java
@@ -1,2 +1,19 @@
 public class Airplane {
+    int numberOfSeatsClass1;
+    int numberOfSeatsClassBusiness;
+    int numberOfSeatsClassEconom;
+
+    enum airplaneModel {Boing737, Boing747, Boing777}
+
+    public Airplane(int numberOfSeatsClass1, int numberOfSeatsClassBusiness, int numberOfSeatsClassEconom) {
+        this.numberOfSeatsClass1 = numberOfSeatsClass1;
+        this.numberOfSeatsClassBusiness = numberOfSeatsClassBusiness;
+        this.numberOfSeatsClassEconom = numberOfSeatsClassEconom;
+    }
+
+    @Override
+    public String toString() {
+        return " Мест в 1м классе" + this.numberOfSeatsClass1 + " Мест в бизнес классе" + this.numberOfSeatsClassBusiness + " Мест в эконом класе" + this.numberOfSeatsClassEconom;
+    }
 }
+
