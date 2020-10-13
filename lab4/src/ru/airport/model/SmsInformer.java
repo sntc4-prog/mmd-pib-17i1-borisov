d65951c915591334fb8237e706fb2127f671a13d
Egor Borisov <egorborisovzypo@gmail.com>
2020-10-13 09:59:01 +0600

    Refactored SmsInformer

--- lab4/src/ru/airport/model/SmsInformer.java
+++ lab4/src/ru/airport/model/SmsInformer.java
@@ -3,11 +3,11 @@ package ru.airport.model;
 public class SmsInformer implements Informer {
     @Override
     public void informLanding(Flight flight, Runway runway) {
-
+        System.out.println("/SMS!/Прибыл рейс:" + flight.getFlightNumber());
     }
 
     @Override
     public void informTakeOff(Flight flight, Runway runway) {
-
+        System.out.println("/SMS!/Рейс снят!:" + flight.getFlightNumber());
     }
 }
